55881fd556d54ee3681e30354c5de3c93798ae65
Aakash Soni <inos92@gmail.com>
2023-04-23 17:02:27 -0500

    added HTML to ProductsShow, created props for show product

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,9 +2,12 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { ProductsIndex } from "./ProductsIndex";
 import { ProductsNew } from "./ProductsNew";
+import { Modal } from "./Modal";
+import { ProductsShow } from "./ProductsShow";
 
 export function Content() {
   const [products, setProducts] = useState([]);
+  const [currentProduct, setCurrentProduct] = useState([]);
 
   const handleProductsIndex = () => {
     axios.get("http://localhost:3000/products.json").then((response) => {
@@ -13,6 +16,9 @@ export function Content() {
     });
   };
 
+  const handleProductsShow = (product) => {
+    setCurrentProduct(product);
+  };
   const handleProductsNew = (params) => {
     axios.post(`http://localhost:3000/products.json`, params).then((response) => {
       console.log(response);
@@ -26,6 +32,9 @@ export function Content() {
     <div>
       <ProductsNew onCreateProduct={handleProductsNew} />
       <ProductsIndex products={products} />
+      <Modal>
+        <ProductsShow product={currentProduct} />
+      </Modal>
     </div>
   );
 }
---
+++ src/ProductsShow.jsx
@@ -0,0 +1,7 @@
+export function ProductsShow(props) {
+  return (
+    <div>
+      <h2>{props.product.name}</h2>
+    </div>
+  );
+}
