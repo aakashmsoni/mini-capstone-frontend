5caae5d627d2cf3c9b847940c974673253d07c65
Aakash Soni <inos92@gmail.com>
2023-04-23 15:45:03 -0500

    updated Content w/ axios, updated props and ProductsIndex

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,22 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { ProductsIndex } from "./ProductsIndex";
+
 export function Content() {
+  const [products, setProducts] = useState([]);
+
+  const handleProductsIndex = () => {
+    axios.get("http://localhost:3000/products.json").then((response) => {
+      console.log(response);
+      setProducts(response.data);
+    });
+  };
+
+  useEffect(handleProductsIndex, []);
+
   return (
     <div>
-      <h1>Hello!</h1>
+      <ProductsIndex products={products} />
     </div>
   );
 }
--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,3 +1,14 @@
-<div>
-  <h1></h1>
-</div>;
+export function ProductsIndex(props) {
+  console.log(props);
+  return (
+    <div id="products-index">
+      <h1>All Products</h1>
+      {props.products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <img src={product.images[0].url} />
+        </div>
+      ))}
+    </div>
+  );
+}
